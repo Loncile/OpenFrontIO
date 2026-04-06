0a117aead3251acd3ba85d9a9ae2c65d49232f0e
evanpelle <evanpelle@gmail.com>
2026-04-06 14:05:50 -0700

    add adfree description to cosmetics

--- src/client/components/CosmeticInfo.ts
+++ src/client/components/CosmeticInfo.ts
@@ -53,6 +53,9 @@ export class CosmeticInfo extends LitElement {
                 ${translateText(`cosmetics.${this.rarity}`) || this.rarity}
               </div>`
             : nothing}
+          <div class="text-green-400 font-bold">
+            ${translateText("cosmetics.adfree")}
+          </div>
           ${this.colorPalette
             ? html`<div>
                 ${translateText("cosmetics.color_label")}
